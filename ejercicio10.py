5ec0aed62254986620e951ace828a7bbe729333c
DanielaRocio <danielle2308scarlet@gmail.com>
2026-04-02 17:35:00 -0400

    Update student information in ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,3 +1,3 @@
-# ESTUDIANTE : JOSE ANTONIO CRUZ ALCON PARALELO: 7C MATERIA: EMERGENTES II
+# ESTUDIANTE : DANIELA ROCIO MAMANI PERALTA RU: 15006398 PARALELO: 7C NOCHE
 numeros = (1, 2, 3, 4, 5, 6)
 print(numeros[3]) # Accede al elemento en la posición 3 (imprime 4)
